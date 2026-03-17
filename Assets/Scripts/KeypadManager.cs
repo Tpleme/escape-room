739199ec5f833c443075a530bca7a9a51f0f6a1d
Leandro Melo <tpleme@hotmail.com>
2026-03-17 19:40:02 +0000

    progress on poke interaction with keypad

---
+++ Assets/Scripts/KeypadManager.cs
@@ -0,0 +1,11 @@
+using TMPro;
+using UnityEngine;
+
+public class KeypadManager : MonoBehaviour
+{
+    public TextMeshPro codeText;
+
+    public void Press1() {
+        Debug.Log("Pressed 1");
+    }
+}
