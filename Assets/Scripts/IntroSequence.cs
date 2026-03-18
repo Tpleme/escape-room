9b9f8be8fd057cdedfd677b732d5b56ba4e7e2d2
Leandro Melo <tpleme@hotmail.com>
2026-03-18 22:18:35 +0000

    added key and doors animations, solved light bug, added ambient audio

--- Assets/Scripts/IntroSequence.cs
+++ Assets/Scripts/IntroSequence.cs
@@ -6,40 +6,41 @@ using UnityEngine.UI;
 public class IntroSequence : MonoBehaviour {
     public TextMeshProUGUI introText;
     public Image blackScreen;
-    
-    [TextArea(3,5)]
+
+    [TextArea(3, 5)]
     public string fullMessage = "Eles deixaram-te para trás. A cabeça pesa-te, os sentidos falham, mas o instinto de sobrevivência grita mais alto. Onde quer que estejas, este não é o teu lugar.\nSai daí.";
     public float letterDelay = 0.05f;
     public float pontuationDelay = 1.0f;
     public float textFadeDuration = 2.0f;
     public float imageFadeDuration = 3.0f;
 
-    [Header("Audio")]
     public AudioSource doorSound;
+    public AudioSource ambientMusic;
 
+    public float ambientMusicVolume = 0.2f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
         introText.text = "";
         introText.alpha = 1f;
-        
+
         Color tempColor = blackScreen.color;
         tempColor.a = 1f;
         blackScreen.color = tempColor;
-        
+
         StartCoroutine(StartIntro());
     }
 
     IEnumerator StartIntro() {
         yield return new WaitForSeconds(2.0f);
 
-        if(doorSound != null) doorSound.Play();
+        if (doorSound != null) doorSound.Play();
 
-        yield return new WaitForSeconds(4.0f);
+        yield return new WaitForSeconds(3.0f);
 
         foreach (char letter in fullMessage.ToCharArray()) {
             introText.text += letter;
-            
+
             float delay = letter == '.' ? pontuationDelay : letterDelay;
 
             yield return new WaitForSeconds(delay);
@@ -48,21 +49,27 @@ public class IntroSequence : MonoBehaviour {
         yield return new WaitForSeconds(2.0f);
 
         float timer = 0;
-        while (timer < textFadeDuration)
-        {
+        while (timer < textFadeDuration) {
             timer += Time.deltaTime;
             introText.alpha = Mathf.Lerp(1f, 0f, timer / textFadeDuration);
+
+            if (ambientMusic != null) {
+                ambientMusic.Play();
+                ambientMusic.volume = Mathf.Lerp(0f, ambientMusicVolume, timer / textFadeDuration);
+            }
+
             yield return null;
         }
+
         introText.alpha = 0f;
 
         timer = 0;
-        while (timer < imageFadeDuration)
-        {
+        while (timer < imageFadeDuration) {
             timer += Time.deltaTime;
             Color c = blackScreen.color;
             c.a = Mathf.Lerp(1f, 0f, timer / imageFadeDuration);
             blackScreen.color = c;
+
             yield return null;
         }
 
